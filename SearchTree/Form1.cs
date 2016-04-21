4eb89f5defa1ad6d4df0aefeb24823e1d3245f01
JanJungbluth <j4n.jungbluth@gmail.com>
2016-04-21 17:31:32 +0200

    Ein paar sachen hinzugefügt

--- SearchTree/Form1.cs
+++ SearchTree/Form1.cs
@@ -15,7 +15,10 @@ namespace SearchTree
     public partial class Form1 : Form
     {
         #region Global Definitions
-        private int DepthCounter = 1;
+        //this is the number of expandsteps
+        private int DepthCounter = 0;
+        private StateSpace Target;
+        private StateSpace TargetFound;
         //create a new form for the viewer
         System.Windows.Forms.Form Graphform = new System.Windows.Forms.Form();
         //create a viewer object 
@@ -30,6 +33,7 @@ namespace SearchTree
         private List<Action> FullActionSet = new List<Action>();
         //create a list for all applicable actions in a given state
         private List<Action> ApplicableActionSet = new List<Action>();
+        
         #endregion
 
         public Form1()
@@ -40,23 +44,12 @@ namespace SearchTree
 
         private void button1_Click(object sender, EventArgs e)
         {
-            #region Some text output
+           
             // Show a new iteration step in the textbox1
             this.textBox1.AppendText(Environment.NewLine
                 + String.Format("-------- Stepnumber {0} ------- ",this.DepthCounter++ )
                 + Environment.NewLine);
-            // Show the iteration setp in the textbox 2
-            this.textBox2.Text = this.DepthCounter.ToString();
-            this.textBox2.Refresh();
-            // Show the number of nodes that exists
-            this.textBox3.Text = this.AllStates.Count.ToString();
-            this.textBox3.Refresh();
-            //Show the number of nodes which are waiting to expan
-            this.textBox4.Text = this.StatesWaitingToExpand.Count.ToString();
-            this.textBox3.Refresh();
-            this.Refresh();
-            #endregion
-
+        
             //copy the StateWaitToExpand list to manipulate the list not in the foreach loop
             List<StateSpace> Temp = new List<StateSpace>(this.StatesWaitingToExpand);
             // Now we have to expand each state in the StatesWaitToExpand List and expand them
@@ -64,12 +57,6 @@ namespace SearchTree
             {
                 // create the new applicable action list ...
                 this.ApplicableActionSet = Helper.PossibleActionSet(this.FullActionSet, CurState);
-                #region more text output
-                // ..and print them to the textbox
-                this.textBox1.AppendText(Environment.NewLine + "Applicable Action List" + Environment.NewLine);
-                foreach (Action CurAction in this.ApplicableActionSet)
-                    this.textBox1.AppendText(CurAction.Name + Environment.NewLine);
-                #endregion
 
                 foreach (Action CurAction in this.ApplicableActionSet)
                 {
@@ -79,6 +66,7 @@ namespace SearchTree
                     if (Helper.IsTargetState(this.AllStates,NewState).Count == 0)
                     {
                         // when there is not that state in the list, then create a new state and add them to the list
+                        // give out the new state
                         // save the new state in the StatesWaitToExpand list
                         this.StatesWaitingToExpand.Add(new StateSpace(NewState));
                         // save the new state in the AllStates list
@@ -87,6 +75,7 @@ namespace SearchTree
                         Microsoft.Msagl.Drawing.Node CurNode = graph.FindNode(CurState.printEnumState());
                         // Add a new node to the graph
                         Microsoft.Msagl.Drawing.Node NewNode = new Microsoft.Msagl.Drawing.Node(NewState.printEnumState());
+                        NewNode.Attr.FillColor = new Microsoft.Msagl.Drawing.Color((byte)NewState.Happiness,0,0);
                         this.graph.AddNode(NewNode);
                         // Add a edge to the graph
                         this.graph.AddEdge(CurNode.Id, CurAction.Name, NewNode.Id);
@@ -115,35 +104,30 @@ namespace SearchTree
                 }
                 
             }
-            // copy all states into the 
-            foreach(StateSpace CurState in this.StatesWaitingToExpand)
-            {
-                this.AllStates.Add(new StateSpace(CurState));
-            }
-            // freeze the form logic
-            Graphform.SuspendLayout();
-            this.tabControl1.SuspendLayout();
-            //bind the graph to the viewer 
-            viewer.Graph = graph;
-            // add the Form control to the tabcontroler
-            this.tabControl1.TabPages[0].Controls.Add(viewer);
-            // doch the viewer with max size to the tabpage
-            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            // de-freeze the form logic
-            Graphform.ResumeLayout();
-            this.tabControl1.TabPages[0].ResumeLayout();
-            // refresh all visual things
-            this.Refresh();
+            //Check if there is a new state in the stateswaittoexpandd list which meets the target state
+            List<StateSpace> Targets = Helper.IsTargetState(this.AllStates, this.Target);
+            if (Targets.Count < 0)
+                foreach (StateSpace CurTarget in Targets)
+                {
+                    this.TargetFound = new StateSpace( CurTarget);
+                    Microsoft.Msagl.Drawing.Node c = graph.FindNode(CurTarget.printEnumState());
+                    c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+                }
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // count the Depthcounter + 1
+            this.DepthCounter++;
             // The first step is to generate the target and start state
             StateSpace StartState = Helper.createStartState();
             //Add the first state to the AllStates list
             this.AllStates.Add(StartState);
             //Generate the target
             StateSpace TargetState = Helper.createTargetState();
+            // and save it in this variable
+            this.Target = new StateSpace(TargetState);
             // We print them both to the textbox
             this.textBox1.AppendText("StartState: " + StartState.printEnumState()+Environment.NewLine);
             this.textBox1.AppendText("TargetState: " + TargetState.printEnumState()+Environment.NewLine);
@@ -216,6 +200,55 @@ namespace SearchTree
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Show the iteration setp in the textbox 2
+            this.textBox2.Text = this.DepthCounter.ToString();
+            this.textBox2.Refresh();
+            // Show the number of nodes that exists
+            this.textBox3.Text = this.AllStates.Count.ToString();
+            this.textBox3.Refresh();
+            //Show the number of nodes which are waiting to expan
+            this.textBox4.Text = this.StatesWaitingToExpand.Count.ToString();
+            this.textBox4.Refresh();
+            //Show the number of node in the graph
+            this.textBox5.Text = this.graph.NodeCount.ToString();
+            this.textBox5.Refresh();
+            //Show the number of edges in the graph
+            this.textBox6.Text = this.graph.EdgeCount.ToString();
+            this.textBox6.Refresh();
+            //Show the number of goals
+            if(this.TargetFound != null)
+            {
+                this.textBox7.Text = this.TargetFound.getId.ToString();
+                this.textBox7.Refresh();
+            }
+            this.textBox7.Text = "No found";
+            this.textBox7.Refresh();
+            this.Refresh();
+           
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // freeze the form logic
+            Graphform.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            //bind the graph to the viewer 
+            viewer.Graph = graph;
+            viewer.l
+            // add the Form control to the tabcontroler
+            this.tabControl1.TabPages[0].Controls.Add(viewer);
+            // doch the viewer with max size to the tabpage
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            // de-freeze the form logic
+            Graphform.ResumeLayout();
+            this.tabControl1.TabPages[0].ResumeLayout();
+            // refresh all visual things
+            this.Refresh();
         }
     }
 }
